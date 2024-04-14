d66f9fb1b72beffbc236461406f34e3c182ea3cf
Răzvan Sbîngu <razsbng@gmail.com>
2024-04-14 10:46:30 +0300

    implement guess input

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -9,7 +9,40 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState('');
+
+  return <WordInput guess={guess} setGuess={setGuess} />;
+}
+
+function WordInput({ guess, setGuess }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log('Submitted guess:', guess);
+
+    setGuess('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="guess-input-wrapper">
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          type="text"
+          id="guess-input"
+          name="guess-input"
+          title="Every guess must have 5 letters"
+          value={guess}
+          required={true}
+          pattern={'[a-zA-Z]{5,5}'}
+          maxLength={5}
+          onChange={(event) =>
+            setGuess(event.target.value.toUpperCase())
+          }
+        />
+      </div>
+    </form>
+  );
 }
 
 export default Game;
