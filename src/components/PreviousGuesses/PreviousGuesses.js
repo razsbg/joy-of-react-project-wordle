c3cf9dcf2592010c407527e8d8a27e5a9790db00
Răzvan Sbîngu <razsbng@gmail.com>
2024-04-19 00:07:22 +0300

    refactor: Remove PreviousGuesses component (#4)
    Removed the PreviousGuesses component and its index export to streamline code.

--- src/components/PreviousGuesses/PreviousGuesses.js
+++
@@ -1,9 +0,0 @@
-function PreviousGuesses({ previousGuesses }) {
-  return previousGuesses.map((guess, index) => (
-    <p key={index} className="guess">
-      {guess}
-    </p>
-  ));
-}
-
-export default PreviousGuesses;
